9a02d473001f796ef2c2bd5b71c410225a89c8d6
Mhjba <mahjoubaaitalla15@gmail.com>
2024-07-23 10:29:48 +0200

    caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Basic Cache"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Basic Caching with put and get methods"""
+
+    def __init__(self):
+        """Initialize the class"""
+        BaseCaching.__init__(self)
+
+    def put(self, key, item):
+        """Assign to the dictionary self.cache_data the item value for the key."""
+        if key is None or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Return the value in self.cache_data linked to key."""
+        if key is not None and key in self.cache_data.keys():
+            return self.cache_data[key]
+        return None
